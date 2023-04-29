95b3bbc741648d144950b352d7fa281f9f443cb2
jerrxcc <2234080520@qq.com>
2023-04-29 23:51:52 +0800

    add code

--- task1/main.c
+++ task1/main.c
@@ -1,8 +1,36 @@
 #include <stdio.h>
-#include <stdlib.h>
 
-int main(int argc, char *argv[]) {
-	printf("Hello World!\n");
-	return 0;
+int main()
+{
+    double y = 0;
+    double a = 0;
+    double h = 0.001;
+    double dy = 0;
+    double x = 0;
+
+    printf("请输入a的值：");
+    scanf("%lf", &a);
+
+    y = a;
+
+    printf("初始值为：%lf\n", y);
+
+    while (1)
+    {
+        dy = (-y * h);
+        y += dy;
+        x += h ;
+
+        printf("x = %lf , y = %lf\n ", x , y);
+
+        if (dy<=0.000001 && dy>=-0.000001)
+        {
+            break;
+        }
+    }
+
+    printf("最终定态（收敛值）为：%lf\n", y);
+
+    return 0;
 }
 
